0fbb3afe1a334856577b1c78ea92667937b409ce
Guilherme <guilherme.chaves@terceirizados.farmarcas.com.br>
2026-07-05 23:08:15 -0300

    a

--- app/Domain/Product/Enums/ProductCategory.php
+++ app/Domain/Product/Enums/ProductCategory.php
@@ -27,6 +27,9 @@ enum ProductCategory: string
     case Case_ = 'case';
     case Accessory = 'accessory';
 
+    // Paraguai
+    case PY = 'py';
+
     // Serviços
     case Service = 'service';
 
@@ -46,6 +49,7 @@ enum ProductCategory: string
             self::Cable => 'Cabo',
             self::Case_ => 'Capinha',
             self::Accessory => 'Acessório',
+            self::PY => 'Paraguai',
             self::Service => 'Serviço',
         };
     }
@@ -61,6 +65,7 @@ enum ProductCategory: string
             self::Headphone, self::Speaker, self::Console, self::Camera => 'Eletrônicos',
             self::Perfume => 'Perfumes',
             self::Charger, self::Cable, self::Case_, self::Accessory => 'Acessórios',
+            self::PY => 'Paraguai',
             self::Service => 'Serviços',
         };
     }
@@ -84,6 +89,7 @@ enum ProductCategory: string
             self::Cable => 'M8 12h.01M12 12h.01M16 12h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z',
             self::Case_ => 'M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4',
             self::Accessory => 'M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4',
+            self::PY => 'M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z',
             self::Service => 'M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z M15 12a3 3 0 11-6 0 3 3 0 016 0z',
         };
     }
@@ -114,6 +120,9 @@ enum ProductCategory: string
                 self::Cable,
                 self::Case_,
                 self::Accessory,
+            ],
+            'Paraguai' => [
+                self::PY,
             ],
             'Outros' => [
                 self::Service,
---
+++ database/migrations/2026_07_05_230641_add_py_category_to_products_table.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        DB::statement("ALTER TABLE products MODIFY COLUMN category ENUM('smartphone','tablet','notebook','smartwatch','headphone','speaker','console','camera','perfume','charger','cable','case','accessory','py','service') NOT NULL DEFAULT 'smartphone'");
+    }
+
+    public function down(): void
+    {
+        DB::statement("ALTER TABLE products MODIFY COLUMN category ENUM('smartphone','tablet','notebook','smartwatch','headphone','speaker','console','camera','perfume','charger','cable','case','accessory','service') NOT NULL DEFAULT 'smartphone'");
+    }
+};
